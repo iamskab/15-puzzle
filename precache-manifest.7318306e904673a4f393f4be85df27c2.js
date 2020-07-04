51040416591ee7a261afbae5f2dfa5c6d684da15
iamskab <iamsourav470@gmail.com>
2020-07-04 21:41:01 +0530

    Updates

---
+++ precache-manifest.7318306e904673a4f393f4be85df27c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649264d6ce68c228af9e4e885425937b",
+    "url": "/15-puzzle/index.html"
+  },
+  {
+    "revision": "68463ff1fbb8e5f7e5c9",
+    "url": "/15-puzzle/static/css/main.e7b14318.chunk.css"
+  },
+  {
+    "revision": "104b01f016be279b017d",
+    "url": "/15-puzzle/static/js/2.b14b4ef3.chunk.js"
+  },
+  {
+    "revision": "d28dc60fafe1b282d7f17900811f45bc",
+    "url": "/15-puzzle/static/js/2.b14b4ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68463ff1fbb8e5f7e5c9",
+    "url": "/15-puzzle/static/js/main.0ef28c35.chunk.js"
+  },
+  {
+    "revision": "cb2db6f783cf51b2eed5",
+    "url": "/15-puzzle/static/js/runtime-main.f26adddb.js"
+  },
+  {
+    "revision": "3bb97edd99398c32429cf4ecda7e0799",
+    "url": "/15-puzzle/static/media/bg1.3bb97edd.jpg"
+  }
+]);
